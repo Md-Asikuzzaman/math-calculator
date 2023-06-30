60dc6de45281addae7ad14224de2000a97e79f37
Md Asikuzzaman <mdasikuzzaman.en@gmail.com>
2023-06-30 11:48:13 +0600

    added all files and folders

---
+++ src/app.ts
@@ -0,0 +1,37 @@
+let display = document.querySelector('.display') as HTMLInputElement;
+let buttons = document.querySelectorAll('button');
+let string: string = '';
+
+buttons.forEach((button) => {
+  button.addEventListener('click', (e: MouseEvent) => {
+    const target = e.target as Element;
+    if (target.innerHTML == '=') {
+      try {
+        if (eval(string) == undefined) {
+          display.value = '0';
+          string = '';
+        } else {
+          string = eval(string);
+          display.value = string;
+        }
+      } catch (error) {
+        display.value = 'Invalid Logic!';
+        string = '';
+      }
+    } else if (target.innerHTML == 'AC') {
+      string = '';
+      display.value = '0';
+    } else if (target.innerHTML == 'DE') {
+      try {
+        string = string.substring(0, string.length - 1);
+        display.value = string;
+      } catch (error) {
+        string = '';
+        display.value = string;
+      }
+    } else {
+      string += target.innerHTML;
+      display.value = string;
+    }
+  });
+});
